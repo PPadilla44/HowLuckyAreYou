59f59a6b51f7aa56e73cbb50d8103d99695f88ae
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-03-09 14:47:02 -0800

    setting up tests

---
+++ components/SubContainer/SubContainer.test.ts
@@ -0,0 +1,9 @@
+// import React from "react";
+// import renderer from "react-test-renderer";
+// import SubContainer from "./SubContainer";
+
+// describe('<SubContainer/>', () => {
+//     it("does trhis", () => {
+//         const tree = renderer.create(<SubContainer  />).toJSON()
+//     })
+// })
