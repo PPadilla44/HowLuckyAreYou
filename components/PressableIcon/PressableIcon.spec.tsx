0b71979e9268ae22d50c4989d35680928fa8fc67
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-03-12 09:42:19 -0800

    Added tests for PressableIcon

---
+++ components/PressableIcon/PressableIcon.spec.tsx
@@ -0,0 +1,33 @@
+import { cleanup, fireEvent, render, RenderAPI } from "@testing-library/react-native";
+import PressableIcon from "./PressableIcon";
+
+describe('<PressableIcon', () => {
+
+    afterEach(cleanup);
+
+    let wrapper: RenderAPI;
+    let mockFn = jest.fn()
+    beforeEach(() => {
+        wrapper = render(<PressableIcon name="gear" size={40} type="evilicon" callBack={mockFn} />)
+    })
+
+    it("renders correctly", () => {
+        wrapper.getByTestId("pressableIconBtn");
+        wrapper.getByTestId("pressableIcon");
+        wrapper.container.findByProps({name: "gear"});
+        wrapper.container.findByProps({ type: "evilicon" })
+        wrapper.container.findByProps({ size: 40 })
+    });
+    
+    it("runs call back function on button press", () => {
+        const btn = wrapper.getByTestId("pressableIconBtn");
+        fireEvent.press(btn);
+        wrapper.debug()
+        expect(mockFn).toBeCalled();
+    });
+
+    it("Matches Snapshot", () => {
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+
+});
--- components/PressableIcon/PressableIcon.tsx
+++ components/PressableIcon/PressableIcon.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet } from 'react-native'
+import { Pressable } from 'react-native'
 import React, { FC } from 'react'
 import { Icon } from '../Themed'
 
@@ -12,11 +12,13 @@ interface Props {
 const PressableIcon: FC<Props> = ({ callBack, name, size, type }) => {
     return (
         <Pressable
+            testID='pressableIconBtn'
             onPress={callBack}
             style={({ pressed }) => ({
-                opacity: pressed ? 0.3 : 1,
+                opacity: pressed ? 0.3 : 1
             })}>
             <Icon
+                testID='pressableIcon'
                 name={name}
                 type={type}
                 size={size}
@@ -26,5 +28,3 @@ const PressableIcon: FC<Props> = ({ callBack, name, size, type }) => {
 }
 
 export default PressableIcon
-
-const styles = StyleSheet.create({})
